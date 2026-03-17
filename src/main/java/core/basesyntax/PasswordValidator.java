91729669746cbefc6ec3a766d2c801c5ae4c8a16
Kovall-Nick <dworak.niko@gmail.com>
2026-03-17 22:44:13 +0200

    Fix line length in PasswordValidator

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 public class PasswordValidator {
     private static final int PASSWORD_LENGTH = 10;
 
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password == null || password.length() < PASSWORD_LENGTH
                 || !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
